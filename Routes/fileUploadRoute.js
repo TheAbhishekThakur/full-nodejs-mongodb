70a76edd7d0cc2db266f694297635a3ef5482f68
TheAbhishekThakur <abhishek84411@gmail.com>
2022-12-18 21:52:37 +0530

    file upload on firebase

--- Routes/fileUploadRoute.js
+++ Routes/fileUploadRoute.js
@@ -1,32 +1,81 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const { upload } = require("../Utils/multer");
+const {
+  FirebaseMulter,
+  uploadFileOnFirebase,
+} = require("../Utils/multerFirebase");
 const { sendResponse, sendErrorResponse } = require("../Utils/reqResFormat");
 
 router.use(express.static(__dirname + "./uploads/"));
 
-// Upload one file using multer
-router.post("/single-file-upload", upload.single("image"), function (req, res, next) {
+// Upload one file using multer in a server folder
+router.post(
+  "/single-file-upload",
+  upload.single("image"),
+  function (req, res, next) {
     try {
-        let response = req.file.filename;
-        res.status(200).send(sendResponse(response, 0, 'file_uploaded_successfully'));
+      let response = req.file.filename;
+      res
+        .status(200)
+        .send(sendResponse(response, 0, "file_uploaded_successfully"));
     } catch (error) {
-        console.log("error", error);
-        res.status(500).send(sendErrorResponse(error, 2, 'internal_server_error'))
+      console.log("error", error);
+      res
+        .status(500)
+        .send(sendErrorResponse(error, 2, "internal_server_error"));
     }
-});
+  }
+);
 
-
-// Upload multiple file using multer
-router.post("/multiple-file-upload", upload.array("files",4), function (req, res, next) {
+// Upload multiple file using multer in a server folder
+router.post(
+  "/multiple-file-upload",
+  upload.array("files", 4),
+  function (req, res, next) {
     try {
-        let response = req.file;
-        res.status(200).send(sendResponse(response, 0, 'file_uploaded_successfully'));
+      let response = req.file;
+      res
+        .status(200)
+        .send(sendResponse(response, 0, "file_uploaded_successfully"));
     } catch (error) {
-        console.log("error", error);
-        res.status(500).send(sendErrorResponse(error, 2, 'internal_server_error'))
+      console.log("error", error);
+      res
+        .status(500)
+        .send(sendErrorResponse(error, 2, "internal_server_error"));
     }
+  }
+);
 
-});
+// Upload single file using multer on firebase
+router.post(
+  "/single-file-upload-firebse",
+  FirebaseMulter.single("image"),
+  uploadFileOnFirebase,
+  function (req, res) {
+    try {
+      let response = req.file.firebaseUrl;
+      console.log("response", response);
+      res
+        .status(200)
+        .send(sendResponse(response, 0, "file_uploaded_successfully"));
+    } catch (error) {
+      console.log("error", error);
+      res
+        .status(500)
+        .send(sendErrorResponse(error, 2, "internal_server_error"));
+    }
+  }
+);
+
+// Upload multiple file using multer on firebase
+
+// Upload single file using multer on cloudinary
+
+// Upload mutiple file using multer on cloudinary
+
+// Upload single file using multer on AWS s3 bucket
+
+// Upload mutiple file using multer on AWS s3 bucket
 
 module.exports = router;
---
+++ Utils/multerFirebase.js
@@ -0,0 +1,55 @@
+// Firebase Setup
+const admin = require("firebase-admin");
+const serviceAccount = require("../Config/nodejs-multer-firebase-firebase-adminsdk-aq8jz-7fc604db46.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: "gs://nodejs-multer-firebase.appspot.com",
+});
+
+const firebaseBucket = admin.storage().bucket();
+// console.log("firebaseBucket", firebaseBucket);
+
+const uploadFileOnFirebase = (req, res, next) => {
+  if (!req.file) {
+    return next();
+  }
+
+  const image = req.file;
+  const fileName = Date.now() + "." + image.originalname.split(".").pop();
+  const file = firebaseBucket.file(fileName);
+  const stream = file.createWriteStream({
+    metaData: {
+      contentType: image.mimeType,
+    },
+  });
+
+  stream.on("error", (e) => {
+    console.log(e);
+  });
+
+  stream.on("finish", async () => {
+    try {
+      await file.makePublic();
+      req.file.firebaseUrl = `${firebaseBucket.storage.apiEndpoint}/${firebaseBucket.name}/${fileName}`;
+      next();
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  stream.end(image.buffer);
+};
+
+// Multer Setup
+const multer = require("multer");
+const FirebaseMulter = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 1000000, // 1000000 Bytes = 1 MB
+  },
+});
+
+module.exports = {
+  FirebaseMulter,
+  uploadFileOnFirebase,
+};
